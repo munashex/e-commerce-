0b26d223cccae4fca6eb578522a89c5d718ab9be
munashex <munashemugondaa@gmail.com>
2023-06-08 13:06:30 +0200

    adding client

---
+++ client/src/App.jsx
@@ -0,0 +1,18 @@
+
+
+const App = () => {
+
+  return (
+    <div> 
+<header className="bg-gray-700 p-3">
+  <a href="/" className="text-white font-bold">munashe store</a>
+</header> 
+<main>
+  list of products
+</main>
+    </div>
+  )
+}
+ 
+
+export default App
